b724d9a7b66473a3f59398c2281be99f447ba1ba
Rajesh-Gour <rajmca2005@gmail.com>
2020-05-20 00:08:28 -0400

    initial commit 1

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+//using DatingApp.API.Models;
+namespace DatingApp.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+
+        }
+
+    public DbSet<Value> Values {get;set;}
+    public DbSet<User> Users{ get; set; }
+
+    
+    
+    }
+
+    internal static class DbSetExtensions
+{
+    public static IAsyncEnumerable<T> AsAsyncEnumerable<T>(this DbSet<T> dbSet)
+        where T : class
+    {
+        return dbSet;
+    }
+
+    public static IQueryable<T> AsQueryable<T>(this DbSet<T> dbSet)
+        where T : class
+    {
+        return dbSet;
+    }
+}
+}
