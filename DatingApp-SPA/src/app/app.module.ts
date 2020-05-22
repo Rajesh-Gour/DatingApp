9b2cd10cf5478497d95522ac3f7a48383ab5f113
Rajesh-Gour <rajmca2005@gmail.com>
2020-05-22 13:38:53 -0400

    test commit 1

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { TestComponent } from '.c:/Users/rajmc/Rajesh Gour/Reading Material/UDeMy/DatingApp-SPA/src/test/test.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      TestComponent
    ],
    imports: [
       BrowserModule,
